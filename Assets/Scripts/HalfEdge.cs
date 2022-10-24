209f9908731a1cffa4510d32bc8e516003208f72
dasilvar <remi.dasilva@edu.esiee.fr>
2022-10-24 16:12:28 +0200

    refact: csv

--- Assets/Scripts/HalfEdge.cs
+++ Assets/Scripts/HalfEdge.cs
@@ -26,6 +26,13 @@ namespace HalfEdge
             this.sourceVertex = vertex;
             this.face = face;
         }
+
+        public HalfEdge(Vertex vertex, Face face)
+        {
+            //this.index = index;
+            this.sourceVertex = vertex;
+            this.face = face;
+        }
     }
     public class Vertex
     {
@@ -58,6 +65,12 @@ namespace HalfEdge
         {
             this.index = index;
         }
+
+        public Face(int index, HalfEdge _edge)
+        {
+            this.index = index;
+            this.edge = _edge;
+        }
     }
     public class HalfEdgeMesh
     {
@@ -81,7 +94,7 @@ namespace HalfEdge
             int[] shapes = mesh.GetIndices(0);
 
             MeshTopology meshTopology = mesh.GetTopology(0);
-            
+
             int nVerticesForTopology = meshTopology.Equals(MeshTopology.Triangles) ? 3 : 4;
 
             for (int i = 0; i < shapes.Length / nVerticesForTopology; i++)
@@ -99,7 +112,7 @@ namespace HalfEdge
                 for (int j = 0; j < tempHalfEdges.Count; j++)
                 {
                     HalfEdge currentHF = tempHalfEdges[j];
-                    int nextEdgeIndice = j == tempHalfEdges.Count - 1 ? 0 : j + 1; 
+                    int nextEdgeIndice = j == tempHalfEdges.Count - 1 ? 0 : j + 1;
                     int previousEdgeIndice = j == 0 ? tempHalfEdges.Count - 1 : j - 1;
 
                     currentHF.prevEdge = tempHalfEdges[previousEdgeIndice];
@@ -150,8 +163,6 @@ namespace HalfEdge
                 this.edges.Add(hf_1);
                 this.edges.Add(hf_2);
                 this.edges.Add(hf_3);*/
-
-
             }
         }
         public Mesh ConvertToFaceVertexMesh()
@@ -187,13 +198,256 @@ namespace HalfEdge
         }
         public string ConvertToCSVFormat(string separator = "\t")
         {
-            string str = "";
-            //magic happens
-            return str;
+            // string str = "";
+            List<string> strings = new List<string>();
+
+            foreach (Vertex vertex in this.vertices)
+            {
+                Vector3 pos = vertex.position;
+                strings.Add(vertex.index + separator +
+                    pos.x.ToString("N02") + " " +
+                    pos.y.ToString("N02") + " " +
+                    pos.z.ToString("N02") + separator +
+                    vertex.outgoingEdge.index + separator + separator);
+            }
+
+            for (int i = 0; i < faces.Count; i++)
+            {
+                Face f = faces[i];
+                strings[i] += f.index + separator + f.edge.index + separator + separator;
+            }
+
+            for (int i = 0; i < this.edges.Count; i++)
+            {
+                HalfEdge he = this.edges[i];
+                strings[i] += he.index + separator + he.sourceVertex.index + separator +
+                he.prevEdge.index + "," + he.nextEdge.index + "," + he.twinEdge + "," +
+                separator + separator;
+            }
+
+            string header = "HalfEdges" + separator + separator + separator + separator + "Faces" +
+                separator + separator + separator + "Vertices\n" +
+                "index" + separator + "sourceVertexIndex" + separator + "edgesIndex" + separator + separator + "index" +
+                separator + "position" + separator + "outgoingEdgeIndex" + "\n";
+
+            return header + string.Join("\n", strings);
         }
+
         public void DrawGizmos(bool drawVertices, bool drawEdges, bool drawFaces)
         {
-            //magic happens
+            if (drawVertices)
+            {
+                foreach (Vertex v in this.vertices)
+                {
+                    Gizmos.DrawSphere(v.position, 0.1f);
+                }
+            }
+            if (drawEdges)
+            {
+                foreach (HalfEdge e in this.edges)
+                {
+                    Gizmos.DrawLine(e.sourceVertex.position, e.nextEdge.sourceVertex.position);
+                }
+            }
+            if (drawFaces)
+            {
+                foreach (Face f in this.faces)
+                {
+                    HalfEdge e = f.edge;
+                    Vector3 p0 = e.sourceVertex.position;
+                    Vector3 p1 = e.nextEdge.sourceVertex.position;
+                    Vector3 p2 = e.nextEdge.nextEdge.sourceVertex.position;
+                    Vector3 p3 = e.nextEdge.nextEdge.nextEdge.sourceVertex.position;
+                    Gizmos.DrawLine(p0, p1);
+                    Gizmos.DrawLine(p1, p2);
+                    Gizmos.DrawLine(p2, p3);
+                    Gizmos.DrawLine(p3, p0);
+                }
+            }
+        }
+
+        /**
+        * Subdivide HalfEdge using Catmull-Clark methods
+        */
+        public void Subdivide()
+        {
+            //Create new vertices
+            List<Vertex> newVertices = new List<Vertex>();
+            foreach (Vertex v in this.vertices)
+            {
+                newVertices.Add(v);
+            }
+            foreach (Face f in this.faces)
+            {
+                HalfEdge e = f.edge;
+                Vector3 p0 = e.sourceVertex.position;
+                Vector3 p1 = e.nextEdge.sourceVertex.position;
+                Vector3 p2 = e.nextEdge.nextEdge.sourceVertex.position;
+                Vector3 p3 = e.nextEdge.nextEdge.nextEdge.sourceVertex.position;
+                Vector3 newVertexPosition = (p0 + p1 + p2 + p3) / 4;
+                Vertex newVertex = new Vertex(newVertices.Count, newVertexPosition);
+                newVertices.Add(newVertex);
+            }
+            foreach (HalfEdge e in this.edges)
+            {
+                Vector3 p0 = e.sourceVertex.position;
+                Vector3 p1 = e.nextEdge.sourceVertex.position;
+                Vector3 p2 = e.nextEdge.nextEdge.sourceVertex.position;
+                Vector3 p3 = e.nextEdge.nextEdge.nextEdge.sourceVertex.position;
+                Vector3 newVertexPosition = (p0 + p1 + p2 + p3) / 4;
+                Vertex newVertex = new Vertex(newVertices.Count, newVertexPosition);
+                newVertices.Add(newVertex);
+            }
+            this.vertices = newVertices;
+
+            //Create new faces
+            this.splitFaces();
+
+            //Create new edges
+            List<HalfEdge> newEdges = this.splitEdges();
+
+            // Calculate the new position of the edges
+            for (int i = 0; i < newEdges.Count; i++)
+            {
+                HalfEdge e = newEdges[i];
+                Vertex v0 = e.sourceVertex;
+                Vertex v1 = e.nextEdge.sourceVertex;
+                Vertex v2 = e.nextEdge.nextEdge.sourceVertex;
+                Vertex v3 = e.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v4 = e.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v5 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v6 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v7 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+
+                e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge = v0.outgoingEdge;
+                e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge = v1.outgoingEdge;
+                e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge = v2.outgoingEdge;
+                e.nextEdge.nextEdge.nextEdge.nextEdge = v3.outgoingEdge;
+                e.nextEdge.nextEdge.nextEdge = v4.outgoingEdge;
+                e.nextEdge.nextEdge = v5.outgoingEdge;
+                e.nextEdge = v6.outgoingEdge;
+                e = v7.outgoingEdge;
+            }
+
+            this.edges = newEdges;
+        }
+
+        private List<HalfEdge> splitEdges()
+        {
+            List<HalfEdge> newEdges = new List<HalfEdge>();
+            foreach (HalfEdge e in this.edges)
+            {
+                Vertex v0 = e.sourceVertex;
+                Vertex v1 = e.nextEdge.sourceVertex;
+                Vertex v2 = e.nextEdge.nextEdge.sourceVertex;
+                Vertex v3 = e.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v4 = e.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v5 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v6 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v7 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+
+                HalfEdge e0 = new HalfEdge(v0, e.face);
+                HalfEdge e1 = new HalfEdge(v1, e.face);
+                HalfEdge e2 = new HalfEdge(v2, e.face);
+                HalfEdge e3 = new HalfEdge(v3, e.face);
+                HalfEdge e4 = new HalfEdge(v4, e.face);
+                HalfEdge e5 = new HalfEdge(v5, e.face);
+                HalfEdge e6 = new HalfEdge(v6, e.face);
+                HalfEdge e7 = new HalfEdge(v7, e.face);
+
+                e0.nextEdge = e1;
+                e1.nextEdge = e2;
+                e2.nextEdge = e3;
+                e3.nextEdge = e4;
+                e4.nextEdge = e5;
+                e5.nextEdge = e6;
+                e6.nextEdge = e7;
+                e7.nextEdge = e0;
+
+                newEdges.Add(e0);
+                newEdges.Add(e1);
+                newEdges.Add(e2);
+                newEdges.Add(e3);
+                newEdges.Add(e4);
+                newEdges.Add(e5);
+                newEdges.Add(e6);
+                newEdges.Add(e7);
+            }
+            return newEdges;
+        }
+
+        private void splitFaces()
+        {
+            List<Face> newFaces = new List<Face>();
+            foreach (Face f in this.faces)
+            {
+                HalfEdge e = f.edge;
+                Vertex v0 = e.sourceVertex;
+                Vertex v1 = e.nextEdge.sourceVertex;
+                Vertex v2 = e.nextEdge.nextEdge.sourceVertex;
+                Vertex v3 = e.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v4 = e.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v5 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v6 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+                Vertex v7 = e.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.nextEdge.sourceVertex;
+
+
+                Face f0 = new Face(newFaces.Count);
+                Face f1 = new Face(newFaces.Count + 1);
+                Face f2 = new Face(newFaces.Count + 2);
+                Face f3 = new Face(newFaces.Count + 3);
+
+                HalfEdge e0 = new HalfEdge(v0, f0);
+                HalfEdge e1 = new HalfEdge(v1, f0);
+                HalfEdge e2 = new HalfEdge(v4, f0);
+                HalfEdge e3 = new HalfEdge(v7, f0);
+
+                HalfEdge e4 = new HalfEdge(v1, f1);
+                HalfEdge e5 = new HalfEdge(v2, f1);
+                HalfEdge e6 = new HalfEdge(v5, f1);
+                HalfEdge e7 = new HalfEdge(v4, f1);
+
+                HalfEdge e8 = new HalfEdge(v2, f2);
+                HalfEdge e9 = new HalfEdge(v3, f2);
+                HalfEdge e10 = new HalfEdge(v6, f2);
+                HalfEdge e11 = new HalfEdge(v5, f2);
+
+                HalfEdge e12 = new HalfEdge(v3, f3);
+                HalfEdge e13 = new HalfEdge(v0, f3);
+                HalfEdge e14 = new HalfEdge(v7, f3);
+                HalfEdge e15 = new HalfEdge(v6, f3);
+
+                e0.nextEdge = e1;
+                e1.nextEdge = e2;
+                e2.nextEdge = e3;
+                e3.nextEdge = e0;
+
+                e4.nextEdge = e5;
+                e5.nextEdge = e6;
+                e6.nextEdge = e7;
+                e7.nextEdge = e4;
+
+                e8.nextEdge = e9;
+                e9.nextEdge = e10;
+                e10.nextEdge = e11;
+                e11.nextEdge = e8;
+
+                e12.nextEdge = e13;
+                e13.nextEdge = e14;
+                e14.nextEdge = e15;
+                e15.nextEdge = e12;
+
+                f0.edge = e0;
+                f1.edge = e4;
+                f2.edge = e8;
+                f3.edge = e12;
+
+                newFaces.Add(f0);
+                newFaces.Add(f1);
+                newFaces.Add(f2);
+                newFaces.Add(f3);
+            }
+            this.faces = newFaces;
         }
     }
 }
--- Assets/Scripts/WingedEdge.cs
+++ Assets/Scripts/WingedEdge.cs
@@ -63,19 +63,22 @@ namespace WingedEdge
             this.index = index;
         }
     }
+
     public class WingedEdgeMesh
     {
         public List<Vertex> vertices;
         public List<WingedEdge> edges;
         public List<Face> faces;
+
         public WingedEdgeMesh(Mesh mesh)
         {
-            // constructeur prenant un mesh Vertex-Face en paramètre
+            // constructeur prenant un mesh Vertex-Face en parametre
             this.vertices = new List<Vertex>();
             this.edges = new List<WingedEdge>();
             this.faces = new List<Face>();
             Vector3[] meshVertices = mesh.vertices;
 
+            // For each vertex
             for (int i = 0; i < mesh.vertexCount; i++)
             {
                 this.vertices.Add(new Vertex(i, meshVertices[i]));
@@ -84,54 +87,154 @@ namespace WingedEdge
             int[] shapes = mesh.GetIndices(0);
 
             MeshTopology meshTopology = mesh.GetTopology(0);
-            
+
             int nVerticesForTopology = meshTopology.Equals(MeshTopology.Triangles) ? 3 : 4;
             int indexFace = 0;
 
-            Dictionary<long, WingedEdge> mapWingedEdges = new Dictionary<long, WingedEdge>(); 
+            Dictionary<long, WingedEdge> mapWingedEdges = new Dictionary<long, WingedEdge>();
 
-            for (int i = 0; i < shapes.Length / nVerticesForTopology; i++)
-            { 
+            int nbFaces = shapes.Length / nVerticesForTopology;
+            // For each face
+            for (int i = 0; i < nbFaces; i++)
+            {
+                // We create two new faces
                 Face leftFace = new Face(indexFace++);
                 Face rightFace = new Face(indexFace++);
 
+                // Create vertex
                 for (int j = 0; j < nVerticesForTopology - 1; j += 2)
                 {
+                    // Get start and end vertex
                     Vertex vStart = this.vertices[shapes[j]];
                     Vertex vEnd = this.vertices[shapes[j + 1]];
-
+                    // Create winged edge
                     WingedEdge wingedEdge = new WingedEdge(j, vStart, vEnd, leftFace, rightFace);
+                    leftFace.edge = wingedEdge;
+                    rightFace.edge = wingedEdge;
 
-                    long key = j + ((j + 1) << 32); 
+                    long key = j + ((j + 1) << 32);
                     mapWingedEdges.Add(key, wingedEdge);
                 }
 
+                // Connect edges
                 for (int j = 0; j < mapWingedEdges.Count; j++)
                 {
                     KeyValuePair<long, WingedEdge> kp = mapWingedEdges.ElementAt(j);
+                    long key = kp.Key;
+                    WingedEdge wingedEdge = kp.Value;
 
+                    // Get start and end edges
+                    long keyStartCW = key + 1;
+                    long keyStartCCW = key - 1;
+                    long keyEndCW = key + (1 << 32);
+                    long keyEndCCW = key - (1 << 32);
+                    // Set start and end edges
+                    wingedEdge.startCWEdge = mapWingedEdges[keyStartCW];
+                    wingedEdge.endCWEdge = mapWingedEdges[keyEndCW];
+                    wingedEdge.startCCWEdge = mapWingedEdges[keyStartCCW];
+                    wingedEdge.endCCWEdge = mapWingedEdges[keyEndCCW];
+                    // Set start and the end
+                    wingedEdge.startVertex.edge = wingedEdge;
+                    wingedEdge.endVertex.edge = wingedEdge;
                 }
             }
-
-
         }
 
         public Mesh ConvertToFaceVertexMesh()
         {
             Mesh faceVertexMesh = new Mesh();
-            // magic happens
+            Vector3[] vertices = new Vector3[this.edges.Count * 2];
+            int[] quads = new int[this.faces.Count * 4];
+
+            foreach (Face face in this.faces)
+            {
+                WingedEdge edge = face.edge;
+                WingedEdge startEdge = edge;
+                do
+                {
+                    vertices[edge.index * 2] = edge.startVertex.position;
+                    vertices[edge.index * 2 + 1] = edge.endVertex.position;
+                    quads[edge.index * 4] = edge.index * 2;
+                    quads[edge.index * 4 + 1] = edge.index * 2 + 1;
+
+                    edge = edge.startCWEdge;
+                } while (edge != startEdge);
+            }
             return faceVertexMesh;
         }
+        
         public string ConvertToCSVFormat(string separator = "\t")
         {
-            string str = "";
-            //magic happens
-            return str;
-        }
-        public void DrawGizmos(bool drawVertices, bool drawEdges, bool drawFaces)
+            int tabSize = Mathf.Max(edges.Count, faces.Count, vertices.Count);
 
+            List<string> strings = new List<string>(tabSize);
+
+            // WingedEdges
+            for(int i = 0 ; i < edges.Count ; i++)
+            {
+                WingedEdge we = edges[i];
+                strings[i] += we.index + separator +
+                    we.startVertex.index + ", " + we.endVertex.index + separator +
+                    we.startCWEdge.index + ", " + we.endCWEdge.index + separator +
+                    we.startCCWEdge.index + ", " + we.endCCWEdge.index + separator + separator;
+            }
+
+            // Faces
+            for(int i = 0 ; i < faces.Count ; i++)
+            {
+                Face f = faces[i];
+                strings[i] += f.index + separator +
+                    f.edge.index + separator + separator;
+            }
+
+            // Vertex
+            for(int i = 0 ; i < vertices.Count ; i++)
+            {
+                Vertex v = vertices[i];
+                strings[i] += v.index + separator +
+                    v.position.x.ToString("N03") + " " +
+                    v.position.y.ToString("N03") + " " +
+                    v.position.z.ToString("N03") + " " + separator +
+                    v.edge.index;
+            }
+
+            string header = "WingedEdges" + separator + separator + separator + separator + separator + "Faces" + separator + separator + separator + "Vertices" + "\n"
+                + "Index" + separator + "start+endVertex index" + separator + "CW index" + separator + "CCW index" + separator + separator
+                + "Index" + separator + "WingedEdge index" + separator + separator
+                + "Index" + separator + "Position" + separator + "WingedEdge index" + "\n";
+
+            return header + string.Join("\n", strings);
+        }
+
+        public void DrawGizmos(bool drawVertices, bool drawEdges, bool drawFaces)
         {
-            //magic happens
+            if (drawVertices)
+            {
+                foreach (Vertex vertex in this.vertices)
+                {
+                    Gizmos.DrawSphere(vertex.position, 0.1f);
+                }
+            }
+            if (drawEdges)
+            {
+                foreach (WingedEdge edge in this.edges)
+                {
+                    Gizmos.DrawLine(edge.startVertex.position, edge.endVertex.position);
+                }
+            }
+            if (drawFaces)
+            {
+                foreach (Face face in this.faces)
+                {
+                    WingedEdge edge = face.edge;
+                    WingedEdge startEdge = edge;
+                    do
+                    {
+                        Gizmos.DrawLine(edge.startVertex.position, edge.endVertex.position);
+                        edge = edge.startCWEdge;
+                    } while (edge != startEdge);
+                }
+            }
         }
     }
 }
